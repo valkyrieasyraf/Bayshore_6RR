fd9dbd9da5fd198a526a0cc048c2ce52f3618608
skitzofrenixx <155915836+valkyrieasyraf@users.noreply.github.com>
2025-01-17 23:20:55 +0800

    Update start_server_pm2.bat

--- start_server_pm2.bat
+++ start_server_pm2.bat
@@ -1,3 +1,4 @@
-pm2 start ./dist/index.js --name 6RR -i max --update-env
-pm2 save --force
+pm2 start ./dist/index.js --name 6RR -i max --update-env && pm2 save --force
+pause
 pm2 log --lines 500
+pause
